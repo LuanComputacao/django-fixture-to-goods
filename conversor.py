6187a0ec27eedea0e2cea21c3d75288748e70acf
Luan Santos <luan.santos@crossknowledge.com>
2018-02-16 20:08:08 -0200

    initial structure

---
+++ conversor.py
@@ -0,0 +1,9 @@
+def convert_to_sql(fixture_path, sql_path):
+    """Convert a fixture in JSON format to SQL SCRIPT
+    """
+    pass
+
+def convert_to_model():
+    """Convert a fixture in JSON format to Python code
+    """
+    pass
